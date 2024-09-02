f3ac6f1e664d0b716f792d20aba0475ee8ac15b8
Glauce Alves <glaucemail@gmail.com>
2024-09-01 21:03:10 -0300

    Sibar

--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -1,14 +1,16 @@
+import SocialNetworks from './SocialNetworks'
 import Avatar from '../img/glauce3x4.jpg'
 import '../styles/components/sidebar.sass'
+import InformationContainer from './InformationContainer'
 
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Glauce Alves" />
-      <p className="title">full-stack web developer</p>
-      <p>redes sociais</p>
-      <p>informações do contato</p>
+      <p className="title">Desenvolvedora Full-Stack</p>
+      <SocialNetworks />
+      <InformationContainer />
       <a href="" className="btn">Download currículo</a>
       </aside>
   )
